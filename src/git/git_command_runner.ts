b152dbcbd8d36426a60e91bd6349a78f397310f1
James Cox-Morton <th3james@katsuragi.local>
2020-12-29 14:36:59 +0000

    Define types

---
+++ src/git/git_command_runner.ts
@@ -0,0 +1,9 @@
+export class GitCommandRunner {
+    getGitRoot(filePath: string): string {
+        throw Error("Not implemented")
+    }
+    
+    getOrigin(filePath: string): string {
+        throw Error("Not implemented")
+    }
+}
