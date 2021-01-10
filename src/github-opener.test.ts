e485e4577df845022fa138e7ef6fad190a726d97
James Cox-Morton <th3james@katsuragi.local>
2021-01-10 15:18:58 +0000

    Add logging class

--- src/github-opener.test.ts
+++ src/github-opener.test.ts
@@ -1,7 +1,8 @@
 import "reflect-metadata";
 import test from "ava";
-import { instance, mock, verify, when } from "ts-mockito";
+import { anything, deepEqual, instance, mock, verify, when } from "ts-mockito";
 
+import { Logger } from "./logging/logger";
 import { GitContext } from "./git/git_context";
 import { GitRemote } from "./git/git_remote";
 import { GithubOpener } from "./github-opener";
@@ -29,7 +30,8 @@ test("GithubOpener.openCurrentFile with a valid file and branch opens the correc
   const githubOpener = new GithubOpener(
     instance(mockIdeContext),
     instance(mockGitContext),
-    instance(mockUrlOpener)
+    instance(mockUrlOpener),
+    instance(mock<Logger>())
   );
 
   await githubOpener.openCurrentFileOnGithub();
@@ -38,17 +40,52 @@ test("GithubOpener.openCurrentFile with a valid file and branch opens the correc
   t.pass();
 });
 
-test("GithubOpener.openCurrentFile without a current file logs it does nothing", (t) => {
+test("GithubOpener.openCurrentFile without a current file it logs the error", (t) => {
+  const mockLogger = mock<Logger>();
+
   const mockIdeContext = mock<IdeContext>();
   when(mockIdeContext.getCurrentFile()).thenReturn(null);
 
   const githubOpener = new GithubOpener(
     instance(mockIdeContext),
     instance(mock<GitContext>()),
-    instance(mock<UrlOpener>())
+    instance(mock<UrlOpener>()),
+    instance(mockLogger)
   );
 
   githubOpener.openCurrentFileOnGithub();
 
+  verify(
+    mockLogger.logError(
+      deepEqual(new Error("No current file, can't open on GitHub"))
+    )
+  ).once();
+
+  t.pass();
+});
+
+test("GithubOpener.openCurrentFile given an invalid url logs the error", async (t) => {
+  const mockLogger = mock<Logger>();
+  const theError = new Error("Bad URL");
+
+  const mockIdeContext = mock<IdeContext>();
+  when(mockIdeContext.getCurrentFile()).thenReturn("whatever");
+
+  const mockGitContext = mock<GitContext>();
+  when(mockGitContext.getRemote("whatever")).thenResolve(new GitRemote("a"));
+
+  const mockUrlOpener = mock<UrlOpener>();
+  when(mockUrlOpener.openUrl(anything())).thenReject(theError);
+
+  const githubOpener = new GithubOpener(
+    instance(mockIdeContext),
+    instance(mockGitContext),
+    instance(mockUrlOpener),
+    instance(mockLogger)
+  );
+
+  await githubOpener.openCurrentFileOnGithub();
+
+  verify(mockLogger.logError(theError)).once();
   t.pass();
 });
