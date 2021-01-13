04c938c536708153e9a0fc13de2ce1f2a8355830
James Cox-Morton <th3james@katsuragi.local>
2021-01-13 14:42:03 +0000

    Add branch to URL building

--- src/github_url_builder/github_url_builder.ts
+++ src/github_url_builder/github_url_builder.ts
@@ -1,7 +1,7 @@
 import { GitRemote } from "../git/git_remote";
 
 export class GithubUrlBuilder {
-  buildUrl(gitRemote: GitRemote, filePath: string): string {
-    return `https://github.com/${gitRemote.owner}/${gitRemote.name}/${filePath}`;
+  buildUrl(gitRemote: GitRemote, branch: string, filePath: string): string {
+    return `https://github.com/${gitRemote.owner}/${gitRemote.name}/blob/${branch}/${filePath}`;
   }
 }
