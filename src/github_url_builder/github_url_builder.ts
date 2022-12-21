7f2ca15d28fc40a0503fcf1afd60d06ced6f9dd7
John Maxwell <jgwmaxwell@gmail.com>
2022-12-21 09:14:50 +0100

    Trim values to ensure valid URLs are created

--- src/github_url_builder/github_url_builder.ts
+++ src/github_url_builder/github_url_builder.ts
@@ -2,6 +2,6 @@ import { GitRemote } from "../git/git_remote";
 
 export class GithubUrlBuilder {
   buildUrl(gitRemote: GitRemote, branch: string, filePath: string): string {
-    return `https://github.com/${gitRemote.owner}/${gitRemote.name}/blob/${branch}/${filePath}`;
+    return `https://github.com/${gitRemote.owner.trim()}/${gitRemote.name.trim()}/blob/${branch}/${filePath}`;
   }
 }
