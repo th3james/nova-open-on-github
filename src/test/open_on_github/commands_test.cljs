5a01ec04b0c61a54ad940092e435afbfde008a46
James Cox-Morton <james.cox-morton@arm.com>
2023-09-06 20:32:44 +0100

    Use with-timeout in open-current-file tests

--- src/test/open_on_github/commands_test.cljs
+++ src/test/open_on_github/commands_test.cljs
@@ -3,26 +3,21 @@
     [cljs.core.async :refer [>! chan go timeout alts!]]
     [cljs.test :refer [async deftest is testing]]
     [open-on-github.commands :refer [open-current-file]]
-    [open-on-github.git :refer [build-github-url]]))
+    [open-on-github.git :refer [build-github-url]]
+    [open-on-github.test-helpers :refer [with-timeout]]))
 
 
 (deftest test-open-current-file
   (testing "gets git info, builds url and opens the browser"
     (async done
-           (let [finished-chan (chan 1)
-                 t (timeout 100)
-                 fake-editor :fake-editor
-                 fake-git-info {:branch "master"}
-                 fake-get-git-info (fn [e]
-                                     (go
-                                       (is (= e fake-editor))
-                                       fake-git-info))
-                 fake-open-url (fn [url]
-                                 (is (= url (build-github-url fake-git-info)))
-                                 (go (>! finished-chan true)))]
-             (open-current-file fake-editor fake-get-git-info fake-open-url)
-             (go
-               (let [[_ ch] (alts! [t finished-chan])]
-                 (if (= ch finished-chan)
-                   (done)
-                   (is false "Timeout reached, test failed"))))))))
+           (with-timeout done
+             (fn [finished-chan]
+               (let [fake-editor :fake-editor
+                     fake-git-info {:branch "master"}
+                     fake-get-git-info (fn [e]
+                                         (is (= e fake-editor))
+                                         (go fake-git-info))
+                     fake-open-url (fn [url]
+                                     (is (= url (build-github-url fake-git-info)))
+                                     (go (>! finished-chan true)))]
+                 (open-current-file fake-editor fake-get-git-info fake-open-url)))))))
