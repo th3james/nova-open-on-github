cf447caa76a939186051c8fcae531fe1ac10f814
James Cox-Morton <james.cox-morton@arm.com>
2023-09-07 20:27:37 +0100

    Move assertions to factory

--- src/test/open_on_github/integration_test.cljs
+++ src/test/open_on_github/integration_test.cljs
@@ -8,22 +8,34 @@
     [open-on-github.test-helpers :refer [with-timeout]]))
 
 
+(defn fake-nova-factory
+  [expectations]
+  (reify INova
+    (run-process
+      [_ executable args]
+      (let [expectation (:run-process expectations)]
+        (is (= executable (:executable expectation)))
+        (is (= args (:args expectation)))
+        (go (:result expectation))))
+
+    (open-url
+      [_ url]
+      (let [expectation (:open-url expectations)]
+        (is (= url (:url expectation)))
+        (go (>! (:finished-chan expectation) true))))))
+
+
 (deftest test-integration-open-current-file
   (testing "gets git info, builds url and opens the browser"
     (async done
            (with-timeout done
              (fn [finished-chan]
                (let [fake-editor #js {"document" #js {"path" "fake/path"}}
-                     fake-nova (reify INova
-                                 (run-process
-                                   [_ executable args]
-                                   (is (= executable "git"))
-                                   (is (= args ["rev-parse" "--abbrev-ref" "HEAD"]))
-                                   (go {:status 0 :out ["master\n"]}))
-
-                                 (open-url
-                                   [_ url]
-                                   (is (= url "github.com/master/"))
-                                   (go (>! finished-chan true))))]
+                     fake-nova (fake-nova-factory
+                                 {:run-process {:executable "git"
+                                                :args ["rev-parse" "--abbrev-ref" "HEAD"]
+                                                :result {:status 0 :out ["master\n"]}}
+                                  :open-url {:url "github.com/master/"
+                                             :finished-chan finished-chan}})]
                  (reset! nova fake-nova)
                  (open-current-file fake-editor)))))))
