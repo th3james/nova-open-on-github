751ebc109e03dbcc0903c4a34250a653fe17c890
James Cox-Morton <jamcox01@kr706cyd7q-43444.local>
2023-09-08 16:25:55 +0100

    Pending test for origin getting

--- src/test/open_on_github/integration_test.cljs
+++ src/test/open_on_github/integration_test.cljs
@@ -40,10 +40,13 @@
                (let [fake-editor #js {"document" #js {"path" "fake/path"}}
                      fake-get-branch {:executable "git"
                                       :args ["rev-parse" "--abbrev-ref" "HEAD"]
-                                      :result {:status 0 :out ["master\n"]}}
+                                      :result {:status 0 :out ["cool-branch\n"]}}
+                     fake-get-origin {:executable "git"
+                                      :args ["config", "--get", "remote.origin.url"]
+                                      :result {:status 0 :out ["git@github.com:cool-guy/nice-project.git\n"]}}
                      fake-nova (fake-nova-factory
-                                 {:run-process [fake-get-branch]
-                                  :open-url {:url "github.com/master/"
+                                 {:run-process [fake-get-branch fake-get-origin]
+                                  :open-url {:url "https://github.com/cool-guy/nice-project/blob/cool-branch/"
                                              :finished-chan finished-chan}})]
                  (reset! nova fake-nova)
                  (open-current-file fake-editor)))))))
