3141276225f5fc5d9a5ac872777f202920f69193
James Cox-Morton <james.cox-morton@arm.com>
2023-09-15 16:58:54 +0100

    Make basic get-git-info loop work

--- src/main/open_on_github/git.cljs
+++ src/main/open_on_github/git.cljs
@@ -1,6 +1,6 @@
 (ns open-on-github.git
   (:require
-    [cljs.core.async :refer [<! go]]
+    [cljs.core.async :refer [<! >! go go-loop chan]]
     [clojure.string :as str]
     [open-on-github.dependencies :refer [nova]]
     [open-on-github.nova-interface :refer [run-process]]))
@@ -24,11 +24,34 @@
 (defn get-git-info
   ([editor] (get-git-info editor get-branch get-origin))
   ([editor get-branch-fn get-origin-fn]
-   (go
-     (let [result (<! (get-branch-fn editor))]
-       (if (= (:status result) :ok)
-         {:status :ok :branch (:branch result)}
-         {:status :error :errors {:branch (:error result)}})))))
+   (let [process-chan (chan)
+         result-chan (chan 1)
+         commands [[:branch get-branch-fn]
+                   [:origin-url get-origin-fn]]]
+     (println "Calling git commands")
+     (doseq [command commands]
+       (let [k (first command)
+             cmd-fn (second command)]
+         (go (>! process-chan {:k k :result (<! (cmd-fn editor))}))))
+     (go-loop [git-info {:status :ok}
+               pending-results 2]
+       (if (zero? pending-results)
+         (do
+           (println "\t\treturning git-info " git-info)
+           (>! result-chan git-info))
+         (let [result (<! process-chan)]
+           (println "\tgit-info" git-info)
+           (println "\tresult" result)
+           (if (= (:status (:result result)) :error)
+             (recur (assoc git-info
+                           :status :error
+                           :errors (assoc (:errors git-info)
+                                          (:k result) (:error (:result result))))
+                    (dec pending-results))
+             (recur (assoc git-info
+                           (:k result) (get (:result result) (:k result)))
+                    (dec pending-results))))))
+     result-chan)))
 
 
 (defn parse-url-from-origin
